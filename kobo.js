5eba6a9b5dacdc97b84a7cf9e1a6c7dd4c68fe84
ZXMushroom63 <116805577+ZXMushroom63@users.noreply.github.com>
2022-12-23 19:21:10 +0800

    update

--- kobo.js
+++ kobo.js
@@ -1,2 +1,7 @@
 //NumberPad Module
+const originalMouseDown = ScratchBlocks.FieldNumber.prototype.showEditor_;
+ScratchBlocks.FieldNumber.prototype.showEditor_ = function (...args) {
+this.useTouchInteraction_ = true;
+return originalMouseDown.apple(this, args);
+}
 //Smthing else
